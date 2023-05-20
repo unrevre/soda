561b74256414967743fb2fc4c54960809d63912c
unrevre <unrevre@gmail.com>
2023-05-20 13:18:27 +0800

    messages: display notes through external messages
    message format should be of the form 'note:[subject]|[body]', with a
    combined limit on the subject and body of 255 characters. longer
    messages will be truncated and an ellipsis appended.

--- src/messages.c
+++ src/messages.c
@@ -3,11 +3,27 @@
 #include "soda.h"
 
 #include "daemon.h"
+#include "modules.h"
 
 #include <stdint.h>
 #include <string.h>
+#include <unistd.h>
 
 void handle_message(struct soda_t* soda, const char* buffer, int32_t len) {
     if (!strncmp(buffer, "empty", 6))
         return terminate(soda->opts);
+
+    if (!strncmp(buffer, "note:", 5)) {
+        if (buffer[5] == '\0')
+            return;
+
+        update_module(&soda->modules[NOTE], (void*)&buffer[5]);
+        soda->modules[NOTE].active = 1;
+
+        sleep(12);
+
+        soda->modules[NOTE].active = 0;
+
+        return;
+    }
 }
